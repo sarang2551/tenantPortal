541e3de25d9da190e6c517501a0c620899e47575
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2023-06-11 13:01:48 +0800

    added update logic

--- mongo/mongo_tenant.js
+++ mongo/mongo_tenant.js
@@ -30,20 +30,21 @@ exports.tenantDatabase = class tenantDatabase{
                 var starting_state = [false,false]
                 var progressBar = {}
                 for(let i = 1; i < 5; i++) progressBar[i] = starting_state
-                console.log(progressBar)
+                
                 var document = {
                 "tenantID":"Sarang",
                 "decription":"",
                 "startDate":"",
                 "progressStage":1,
                 "progressBar":progressBar,
-                "endDate":"",
+                "endDate":"", // needs to be updated later
                 "images":[],
                 "documents":[],
                 "title":"",
                 "landlordName":"",
                 "landlordID":"",
-                "id":"testID"
+                "id":"testID",
+                "unitID":""
                 }
                 var landlordID = serviceTicket["landlordID"]
                 // get the landlord object ID in the database to add that to the service Ticket ID
@@ -62,7 +63,7 @@ exports.tenantDatabase = class tenantDatabase{
             }
           
         } catch (error) {
-          console.error('Error adding document:', error);
+          console.error('Error adding document:', error)
           return false
         }
       }
@@ -77,11 +78,22 @@ exports.tenantDatabase = class tenantDatabase{
         var curStage = serviceTicket['progressStage']
         var progressBar = serviceTicket['progressBar']
         var curLevel = progressBar[curStage]
+        var endDate = serviceTicket['endDate']
         if(curLevel[1] == true && curLevel[0] == false){
             // landlord has already approved the curStage
             progressBar[curStage][0] = true
             // current stage of the serviceTicker is completed
-            curStage += 1
+            if(curStage < 4){
+                curStage += 1
+            } else{
+                // the final stage is completed
+                var newDate = new Date()
+                const year = newDate.getFullYear();
+                const month = String(newDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+                const day = String(newDate.getDate()).padStart(2, '0');
+                endDate = `${day}-${month}-${year}`
+            }
+            
         } else if (curLevel[1] == false && curLevel[0] == false) {
             // tenant has approved the current stage while landlord hasn't
             progressBar[curStage][0] = true
@@ -89,8 +101,9 @@ exports.tenantDatabase = class tenantDatabase{
             console.log("tenant has already updated the progress for this stage")
             return false
         }
+        
 
-        collection.updateOne({_id:serviceTicket["_id"]},{$set: { progressBar, progressStage: curStage }},(err,result)=>{
+        collection.updateOne({_id:serviceTicket["_id"]},{$set: { progressBar, progressStage: curStage, endDate }},(err,result)=>{
             if(err){
                 console.log(err)
                 return false
@@ -98,5 +111,34 @@ exports.tenantDatabase = class tenantDatabase{
         })
         return true
     }
+    async deleteServiceTicket(serviceTicketID){
+        const collection = this.database.collection(this.useCases.deleteServiceTicket)
+        await collection.deleteOne({id:serviceTicketID},(err,result)=>{
+            if(err){
+                console.log(`Unable to delete Service Ticket with ID: ${serviceTicketID}`)
+                return false
+            } else {
+                console.log(`Deleted serviceTicket ${serviceTicketID}`)
+                return true
+            }
+        })
+    }
+    async registerUnit(data){
+        // {unitID, buildingID, unitNumber, images, landlordID, tenantID}
+        const collection = this.database.collection(this.useCases.registerUnit)
+        
+        await collection.insertOne(data,(err,result)=>{
+            if(err){
+                console.log(`Unable to registerUnit with ID: ${serviceTicketID}`)
+                return false
+            }
+        })
+
+    }
+    async addLandlord(){
+        // sends a notification to the landlord that the tenant wants to add them
+        // once the status is approved, then the landlord is added automatically
+    }
+   
 
 }
